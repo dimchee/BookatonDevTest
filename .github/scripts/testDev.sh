251931a659627a7ce7ac94a171603e1d024e4dd8
dimchee <41148612+dimchee@users.noreply.github.com>
2023-03-26 04:28:45 +0200

    Update testDev.sh

--- .github/scripts/testDev.sh
+++ .github/scripts/testDev.sh
@@ -1,3 +1,3 @@
 echo "file: $1"
 cat $1
-curl -H "Content-Type: application/json" --data-binary "{\"content\": \"$(cat $1 | base64)\" }" 23.88.64.45
+curl -H "Content-Type: application/json" --data-binary "{\"content\": \"$(base64 $1)\" }" 23.88.64.45
